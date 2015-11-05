1d8c6ca18884f8bc57e9895f5052aca25e678b78
elvis <289014633@qq.com>
2015-11-05 21:31:12 +0800

    折线

--- Computer_Graphics_with_OpenGL/chp_3/lineGraph.h
+++ Computer_Graphics_with_OpenGL/chp_3/lineGraph.h
@@ -15,25 +15,32 @@ GLint dataValue[12] = { 420,342,324,310,262,185,
 
 void init(void)
 {
-	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClearColor(1.0, 1.0, 1.0, 1.0);				// White display window.
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0.0,600.0,0.0,500.0);
 }
 
 void lineGraph(void)
 {
-	GLint month, k;
+	GLint month, k;									//Initialize x position for chart.
 	GLint x = 30;
 
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.0, 0.0, 1.0);
-	glBegin(GL_LINE_STRIP);
+	glClear(GL_COLOR_BUFFER_BIT);					// Clear display window.
+	glColor3f(0.0, 0.0, 1.0);						// Set line color to blue.
+	glBegin(GL_LINE_STRIP);							// Plot data as a polyline.
 	for (k = 0;k < 12;++k)
 		glVertex2i(x + k * 50, dataValue[k]);
 	glEnd();
 
-	glColor3f(0.0, 0.0, 0.0);
-	xRaster = 20;
+	glColor3f(1.0, 0.0, 0.0);						// Set marker color to red.
+	for (k = 0;k < 12; ++k)							// Plot data as asterish polymarkers
+	{
+		glRasterPos2i(xRaster + k * 50, dataValue[k] - 4);
+		glutBitmapCharacter(GLUT_BITMAP_9_BY_15,'*');
+	}
+
+	glColor3f(0.0, 0.0, 0.0);						// Set marker color to red.
+	xRaster = 20;									// Display chart labels.
 	for (month = 0;month < 12;++month)
 	{
 		glRasterPos2i(xRaster, yRaster);
