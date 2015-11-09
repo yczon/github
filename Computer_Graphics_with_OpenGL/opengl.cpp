ad23fa9cba6f15dc28e3207c2bb34b7e6dccb9e2
yczon <289014633@qq.com>
2015-11-09 08:49:34 +0800

    鼠标

--- Computer_Graphics_with_OpenGL/opengl.cpp
+++ Computer_Graphics_with_OpenGL/opengl.cpp
@@ -1,21 +1,62 @@
 #include "stdafx.h"
 #include<GL/glut.h>
 #include<iostream>
-#include"chp_3/barChart.h"
 
 using namespace std;
 
+GLsizei winWidth = 400, winHeight = 300;
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 1.0, 1.0);
+
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0,200.0,0.0,150.0);
+}
+
+void displayFcn(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(3.0);
+}
+
+void winReshapeFcn(GLint newWidth, GLint newHeight)
+{
+	glViewport(0, 0, newWidth, newHeight);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, GLdouble(newWidth), 0.0, GLdouble(newHeight));
+
+	winWidth = newWidth;
+	winHeight = newHeight;
+}
+
+void plotPoint(GLint x, GLint y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x,y);
+	glEnd();
+}
+void mousePtPlot(GLint button, GLint action, GLint xMouse, GLint yMouse)
+{
+	if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN)
+		plotPoint(xMouse, winHeight - yMouse);
+	glFlush();
+}
 void main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(winWidth, winHeight);
-	glutCreateWindow("Line Chart Data Plot");
+	glutCreateWindow("Mouse Plot Points");
 
 	init();
-	glutDisplayFunc(barChart);
+	glutDisplayFunc(displayFcn);
 	glutReshapeFunc(winReshapeFcn);
+	glutMouseFunc(mousePtPlot);
 
 	glutMainLoop();
 }
