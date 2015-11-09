5578565c7beab3b1bcb80cb9553a684f641ee163
elvis <289014633@qq.com>
2015-11-09 20:41:40 +0800

    OpenGl 显示窗口重定型函数

--- Computer_Graphics_with_OpenGL/opengl.cpp
+++ Computer_Graphics_with_OpenGL/opengl.cpp
@@ -1,60 +1,100 @@
-#include "stdafx.h"
+#include"stdafx.h"
 #include<GL/glut.h>
-#include<iostream>
+#include<cmath>
 
-using namespace std;
+const double TWO_PI = 6.2831853;
 
-typedef GLint vertex3[3];
-vertex3 pt[8] = {
-	{0,0,0},
-	{0,100,0},
-	{100,0,0},
-	{100,100,0},
-	{0,0,100},
-	{0,100,100},
-	{100,0,100},
-	{100,100,100},
-};
-void init(void)
+//  Initial display-window size.
+GLsizei winWidth = 400, winHeight = 400;
+GLuint regHex;
+
+class screenPt
 {
+private:
+	GLint x, y;
+public:
+	// Default Constructor: initializes coordinate position to (0,0)
+	screenPt()
+	{
+		x = y = 0;
+	}
+
+	void setCoords(GLint xCoord, GLint yCoord)
+	{
+		x = xCoord;
+		y = yCoord;
+	}
+
+	GLint getx() const {
+		return x;
+	}
+
+	GLint gety() const {
+		return y;
+	}
+};
+
+static void init(void)
+{
+	screenPt hexVertex, circCtr;
+	GLdouble theta;
+	GLint k;
+
+	// Set circle center coordinates.
+	circCtr.setCoords(winWidth / 2, winHeight / 2);
+
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 
-	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+	/* Set up a display list for a red regular hexagon.
+	* Vertices for the hexagon are six equally spaced
+	* points around the circumference of a circle.
+	*/
+	regHex = glGenLists(1);				// Get an identifier for the display list.
+	glNewList(regHex, GL_COMPILE);
+	glColor3f(0.2, 0.4, 0.2);		// Set fill color for hexagon to red.
+	glBegin(GL_POLYGON);
+	for (k = 0;k < 6;k++)
+	{
+		theta = TWO_PI * k / 6.0;
+		hexVertex.setCoords(circCtr.getx() + 150 * cos(theta),
+			circCtr.gety() + 150 * sin(theta));
+		glVertex2i(hexVertex.getx(), hexVertex.gety());
+	}
+	glEnd();
+	glEndList();
 }
 
-void cube()
+void regHexagon(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(0.0, 0.4, 0.2);
-
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_INT, 0, pt);
-	GLubyte vertIndex[] = {
-			6,2,3,7,
-			5,1,0,4,
-			7,3,0,4,
-			4,0,2,6,
-			2,0,1,3,
-			7,5,4,6
-	};
-
-	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, vertIndex);
+	glCallList(regHex);
 
 	glFlush();
 }
+
+void winReshapeFcn(int newWidth, int newHeight)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, (GLdouble)newWidth, 0.0, (GLdouble)newHeight);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+
 void main(int argc, char ** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(50, 100);
-	glutInitWindowSize(400, 300);
-	glutCreateWindow("An Example OpenGL Program");
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(winWidth,winHeight);
+	glutCreateWindow("Reshape-Function & Display-List Exalple");
 
 	init();
 
+	glutDisplayFunc(regHexagon);
+	glutReshapeFunc(winReshapeFcn);
 
-	glutDisplayFunc(cube);
 	glutMainLoop();
 }
