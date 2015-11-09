c3e7c693d8993f2f2f5a2252f4ecb172e5d08b92
yczon <289014633@qq.com>
2015-11-09 17:35:52 +0800

    画点

--- Computer_Graphics_with_OpenGL/opengl.cpp
+++ Computer_Graphics_with_OpenGL/opengl.cpp
@@ -16,10 +16,10 @@ void lineSegment(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	int p1[] = { 50,100 };
 	glColor3f(0.0, 0.4, 0.2);
-	glBegin(GL_LINES);
-	glVertex2i(180, 15);
-	glVertex2i(10, 145);
+	glBegin(GL_POINTS);
+	glVertex2iv(p1);
 	glEnd();
 
 	glFlush();
