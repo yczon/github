707ff3122f262b8695490681d0523fb585e662ea
yczon <289014633@qq.com>
2015-11-09 17:59:23 +0800

    none

--- Computer_Graphics_with_OpenGL/opengl.cpp
+++ Computer_Graphics_with_OpenGL/opengl.cpp
@@ -1,6 +1,20 @@
-#include"stdafx.h"
+#include "stdafx.h"
 #include<GL/glut.h>
+#include<iostream>
 
+using namespace std;
+
+typedef GLint vertex3[3];
+vertex3 pt[8] = {
+	{0,0,0},
+	{0,100,0},
+	{100,0,0},
+	{100,100,0},
+	{0,0,100},
+	{0,100,100},
+	{100,0,100},
+	{100,100,100},
+};
 void init(void)
 {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -9,48 +23,45 @@ void init(void)
 	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
 }
 
-void lineSegment(void)
+void quad(GLint n1, GLint n2, GLint n3, GLint n4)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	int p1[] = { 10,50 };
-	int p2[] = { 20,10 };
-	int p3[] = { 50,20 };
-	int p4[] = { 40,45 };
-	int p5[] = { 80,45 };
-	int p6[] = { 80,20 };
-	int p7[] = { 110,10 };
-	int p8[] = { 110,60 };
-
-	glColor3f(0.2, 0.5, 0.3);
-
-	glBegin(GL_QUAD_STRIP);
-	glVertex2iv(p1);
-	glVertex2iv(p2);
-	glVertex2iv(p4);
-	glVertex2iv(p3);
-
-	glVertex2iv(p5);
-	glVertex2iv(p6);
-
-	glVertex2iv(p8);
-	glVertex2iv(p7);
-
-
+	glColor3f(0.0, 0.4, 0.2);
+	glBegin(GL_LINES);
+	glVertex2i(180, 15);
+	glVertex2i(10, 145);
 	glEnd();
 
 	glFlush();
 }
 
-void main(int argc, char **argv)
+void draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(0.0, 0.4, 0.2);
+	GLubyte bitShape[20] = {
+		0x1c,0x00,0x1c,0x00,0x1c,0x00,0x1c,0x00,0x1c,0x00,
+		0xff,0x80,0x7f,0x00,0x3e,0x00,0x1c,0x00,0x08,0x00
+	};
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glRasterPos2i(30, 40);
+	glBitmap(9, 10, 0.0, 0.0, 20.0, 15.0, bitShape);
+	glFlush();
+}
+void main(int argc, char ** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(50, 100);
 	glutInitWindowSize(400, 300);
-	glutCreateWindow("Title");
+	glutCreateWindow("An Example OpenGL Program");
 
 	init();
-	glutDisplayFunc(lineSegment);
+
+
+	glutDisplayFunc(draw);
 	glutMainLoop();
 }
