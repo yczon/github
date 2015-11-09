9b2aa1eab539cfcb504e231a8dde023fb3f172c4
elvis <289014633@qq.com>
2015-11-09 20:43:31 +0800

    折线

--- Computer_Graphics_with_OpenGL/opengl.cpp
+++ Computer_Graphics_with_OpenGL/opengl.cpp
@@ -1,83 +1,61 @@
 #include"stdafx.h"
 #include<GL/glut.h>
-#include<cmath>
 
-const double TWO_PI = 6.2831853;
+GLsizei winWidth = 600, winHeight = 500;
+GLint xRaster = 25, yRaster = 150;
 
-//  Initial display-window size.
-GLsizei winWidth = 400, winHeight = 400;
-GLuint regHex;
+GLubyte label[36] = { 'J','a','n',	'F','e','b',	'M','a','r',
+'A','p','r',	'M','a','y',	'J','u','n',
+'J','u','l',	'A','u','g',	'S','e','p',
+'O','c','t',	'N','o','v',	'D','e','c' };
 
-class screenPt
+GLint dataValue[12] = { 420,342,324,310,262,185,
+190,196,217,240,312,438 };
+
+void init(void)
 {
-private:
-	GLint x, y;
-public:
-	// Default Constructor: initializes coordinate position to (0,0)
-	screenPt()
-	{
-		x = y = 0;
-	}
-
-	void setCoords(GLint xCoord, GLint yCoord)
-	{
-		x = xCoord;
-		y = yCoord;
-	}
-
-	GLint getx() const {
-		return x;
-	}
-
-	GLint gety() const {
-		return y;
-	}
-};
-
-static void init(void)
-{
-	screenPt hexVertex, circCtr;
-	GLdouble theta;
-	GLint k;
-
-	// Set circle center coordinates.
-	circCtr.setCoords(winWidth / 2, winHeight / 2);
-
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-
-	/* Set up a display list for a red regular hexagon.
-	* Vertices for the hexagon are six equally spaced
-	* points around the circumference of a circle.
-	*/
-	regHex = glGenLists(1);				// Get an identifier for the display list.
-	glNewList(regHex, GL_COMPILE);
-	glColor3f(0.2, 0.4, 0.2);		// Set fill color for hexagon to red.
-	glBegin(GL_POLYGON);
-	for (k = 0;k < 6;k++)
-	{
-		theta = TWO_PI * k / 6.0;
-		hexVertex.setCoords(circCtr.getx() + 150 * cos(theta),
-			circCtr.gety() + 150 * sin(theta));
-		glVertex2i(hexVertex.getx(), hexVertex.gety());
-	}
-	glEnd();
-	glEndList();
+	glClearColor(1.0, 1.0, 1.0, 1.0);				// White display window.
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0, 600.0, 0.0, 500.0);
 }
 
-void regHexagon(void)
+void lineGraph(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	GLint month, k;									//Initialize x position for chart.
+	GLint x = 30;
 
-	glCallList(regHex);
+	glClear(GL_COLOR_BUFFER_BIT);					// Clear display window.
+	glColor3f(0.0, 0.0, 1.0);						// Set line color to blue.
+	glBegin(GL_LINE_STRIP);							// Plot data as a polyline.
+	for (k = 0;k < 12;++k)
+		glVertex2i(x + k * 50, dataValue[k]);
+	glEnd();
+
+	glColor3f(1.0, 0.0, 0.0);						// Set marker color to red.
+	for (k = 0;k < 12; ++k)							// Plot data as asterish polymarkers
+	{
+		glRasterPos2i(xRaster + k * 50, dataValue[k] - 4);
+		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '*');
+	}
+
+	glColor3f(0.0, 0.0, 0.0);						// Set marker color to red.
+	xRaster = 20;									// Display chart labels.
+	for (month = 0;month < 12;++month)
+	{
+		glRasterPos2i(xRaster, yRaster);
+		for (k = 3 * month;k < 3 * month + 3;++k)
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, label[k]);
+		xRaster += 50;
+	}
 
 	glFlush();
 }
 
-void winReshapeFcn(int newWidth, int newHeight)
+void winReshapeFcn(GLint newWidth, GLint newHeight)
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0.0, (GLdouble)newWidth, 0.0, (GLdouble)newHeight);
+	gluOrtho2D(0.0, GLdouble(newWidth), 0.0, GLdouble(newHeight));
 
 	glClear(GL_COLOR_BUFFER_BIT);
 }
@@ -89,11 +67,11 @@ void main(int argc, char ** argv)
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(winWidth,winHeight);
-	glutCreateWindow("Reshape-Function & Display-List Exalple");
+	glutCreateWindow("Line Chart Data Plot");
 
 	init();
 
-	glutDisplayFunc(regHexagon);
+	glutDisplayFunc(lineGraph);
 	glutReshapeFunc(winReshapeFcn);
 
 	glutMainLoop();
