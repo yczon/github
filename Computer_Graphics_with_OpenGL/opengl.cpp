10797233a77738a8f69a286df5fe771d67e21a2e
elvis <289014633@qq.com>
2015-11-09 22:27:00 +0800

    线型，线宽

--- Computer_Graphics_with_OpenGL/opengl.cpp
+++ Computer_Graphics_with_OpenGL/opengl.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+typedef struct
+{
+	float x, y;
+} wcPt2D;
+
+wcPt2D dataPts[5];
+
 void init(void)
 {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -12,25 +19,21 @@ void init(void)
 	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
 }
 
+
 void lineSegment(void)
 {
+	glEnable(GL_LINE_STIPPLE);
+	glLineWidth(10);
+	glLineStipple(1,0x0101);
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	glPointSize(10.0);
 	glColor3f(0.0, 0.4, 0.2);
-
-	glBegin(GL_POINTS);
-	glVertex2i(25,60);
+	int k;
+	glBegin(GL_LINES);
+		glVertex2f(10, 10);
+		glVertex2f(100, 100);
 	glEnd();
-
-	glPointSize(20.0);
-	glColor3f(0.0, 1.0, 0.0);
-
-	glBegin(GL_POINTS);
-	glVertex2i(50,100);
-	glEnd();
-
 	glFlush();
+	glDisable(GL_LINE_STIPPLE);
 }
 
 void main(int argc, char ** argv)
