78e5233b6e0eae186a8d28276c0ada820ac44cae
yczon <289014633@qq.com>
2015-11-09 17:46:19 +0800

    三角形  FAN

--- Computer_Graphics_with_OpenGL/opengl.cpp
+++ Computer_Graphics_with_OpenGL/opengl.cpp
@@ -22,14 +22,14 @@ void lineSegment(void)
 
 	glColor3f(0.2, 0.5, 0.3);
 
-	glBegin(GL_TRIANGLE_STRIP);
+	glBegin(GL_TRIANGLE_FAN);
 	glVertex2iv(p1);
 	glVertex2iv(p2);
-	glVertex2iv(p6);
-
 	glVertex2iv(p3);
-	glVertex2iv(p5);
+
 	glVertex2iv(p4);
+	glVertex2iv(p5);
+	glVertex2iv(p6);
 	glEnd();
 
 	glFlush();
