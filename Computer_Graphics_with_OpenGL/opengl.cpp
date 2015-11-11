18f54b96e7e73625b3818fe5f589d8c3611e2f05
yczon <289014633@qq.com>
2015-11-11 10:35:43 +0800

    用鼠标画点

--- Computer_Graphics_with_OpenGL/opengl.cpp
+++ Computer_Graphics_with_OpenGL/opengl.cpp
@@ -1,42 +1,47 @@
-#include "stdafx.h"
+#include"stdafx.h"
 #include<GL/glut.h>
-#include<iostream>
 
-using namespace std;
-
-typedef struct
-{
-	float x, y;
-} wcPt2D;
-
-wcPt2D dataPts[5];
+GLsizei winWidth = 400, winHeight = 300;
 
 void init(void)
 {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClearColor(0.0, 0.0, 1.0, 1.0);
+
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
 }
 
-
-void lineSegment(void)
+void displayFcn(void)
 {
-	glLineWidth(10);
-	glPolygonMode(GL_FRONT,GL_LINE);
-	glColor3f(0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-		glVertex2i(50,50);
-		glVertex2i(150, 50);
-		glVertex2i(75, 150);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(3.0);
+}
+
+void winReshapeFcn(GLint newWidth, GLint newHeight)
+{
+	glViewport(0, 0, newWidth, newHeight);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, GLdouble(newWidth), 0.0, GLdouble(newHeight));
+
+	winWidth = newWidth;
+	winHeight = newHeight;
+}
+
+void plotPoint(GLint x, GLint y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
 	glEnd();
-	glPolygonMode(GL_FRONT,GL_FILL);
-	glColor3f(0.0, 1.0, 0.0);
-	glBegin(GL_TRIANGLES);
-		glVertex2i(50,50);
-		glVertex2i(150, 50);
-		glVertex2i(75, 150);
-	glEnd();
+}
+
+void mousePtPlot(GLint button, GLint action, GLint xMouse, GLint yMouse)
+{
+	if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN)
+		plotPoint(xMouse, winHeight - yMouse);
+
 	glFlush();
 }
 
@@ -44,13 +49,15 @@ void main(int argc, char ** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(50, 100);
-	glutInitWindowSize(400, 300);
-	glutCreateWindow("An Example OpenGL Program");
+	glutInitWindowPosition(100,100);
+	glutInitWindowPosition(winWidth,winHeight);
+
+	glutCreateWindow("Mouse Plot Points");
 
 	init();
+	glutDisplayFunc(displayFcn);
+	glutReshapeFunc(winReshapeFcn);
+	glutMouseFunc(mousePtPlot);
 
-
-	glutDisplayFunc(lineSegment);
 	glutMainLoop();
 }
