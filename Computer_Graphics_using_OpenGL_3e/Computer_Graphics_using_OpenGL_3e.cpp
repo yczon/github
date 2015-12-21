41001f22ddc2966389125a77c78493f89aa41ac1
yczon <289014633@qq.com>
2015-12-21 11:19:46 +0800

    moveTo ,lineTo

--- Computer_Graphics_using_OpenGL_3e/Computer_Graphics_using_OpenGL_3e.cpp
+++ Computer_Graphics_using_OpenGL_3e/Computer_Graphics_using_OpenGL_3e.cpp
@@ -23,59 +23,30 @@ struct GLintPoint {
 	GLint x, y;
 };
 
-//<<<<<<<<<<<<<<<<<< parameterizeHouse >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-void parameterizeHouse(GLintPoint peak, GLint width, GLint height)
-// 房子的顶点：peak，给定房子的大小：width,height
+GLintPoint CP;
+//<<<<<<<<<<<<<<<<<< moveto >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+void moveTo(GLint x, GLint y)
 {
-	glBegin(GL_LINE_LOOP);										// 画房子的外形
-	glVertex2i(peak.x, peak.y);
-	glVertex2i(peak.x + width, peak.y);
-	glVertex2i(peak.x + width, peak.y + 5 * height / 8);
-	glVertex2i(peak.x + width / 2, peak.y + height);
-	glVertex2i(peak.x, peak.y + 5 * height / 8);
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);										// 画烟囱
-	glVertex2i(peak.x + width / 6, peak.y + 6 * height / 8);
-	glVertex2i(peak.x + width / 6, peak.y + height);
-	glVertex2i(peak.x + width / 3, peak.y + height);
-	glVertex2i(peak.x + width / 3, peak.y + 7 * height / 8);
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);										// 画门
-	glVertex2i(peak.x + width / 6, peak.y);
-	glVertex2i(peak.x + width / 6, peak.y + height / 2);
-	glVertex2i(peak.x + width / 3, peak.y + height / 2);
-	glVertex2i(peak.x + width / 3, peak.y);
-	glEnd();
-
-
-	glBegin(GL_LINE_LOOP);										// 画窗户
-	glVertex2i(peak.x + 4 * width / 6, peak.y + 4 * height / 8);
-	glVertex2i(peak.x + 5 * width / 6, peak.y + 4 * height / 8);
-	glVertex2i(peak.x + 5 * width / 6, peak.y + 5 * height / 8);
-	glVertex2i(peak.x + 4 * width / 6, peak.y + 5 * height / 8);
-	glEnd();
-
-
-
+	CP.x = x;					// 更新CP
+	CP.y = y;
 }
+//<<<<<<<<<<<<<<<<<< lineTo >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+void lineTo(GLint x, GLint y)
+{
+	glBegin(GL_LINES);			// 画线
+	glVertex2i(CP.x,CP.y);
+	glVertex2i(x,y);
+	glEnd();
+	glFlush();
+	CP.x = x;					// 更新CP
+	CP.y = y;
+}
+
 //<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 void myDisplay() {
-	GLintPoint p;
 	glClear(GL_COLOR_BUFFER_BIT);		// 清屏
-	p.x = 10, p.y = 50;
-	parameterizeHouse(p, 50, 70);
-
-	p.x = 60, p.y = 120;
-	parameterizeHouse(p, 30, 40);
-
-	p.x = 100, p.y = 10;
-	parameterizeHouse(p, 30, 80);
-
-	p.x = 200, p.y = 10;
-	parameterizeHouse(p, 90, 70);
-	glFlush();							// 送所有输出到显示设备
+	moveTo(40,40);
+	lineTo(70,123);
 }
 int main(int argc, char **argv)
 {
