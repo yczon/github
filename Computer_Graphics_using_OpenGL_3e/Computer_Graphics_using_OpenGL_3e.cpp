97bb4b6860a271fa8fcfc39a06499a3857518bff
yczon <289014633@qq.com>
2015-12-21 17:22:38 +0800

    鼠标画点

--- Computer_Graphics_using_OpenGL_3e/Computer_Graphics_using_OpenGL_3e.cpp
+++ Computer_Graphics_using_OpenGL_3e/Computer_Graphics_using_OpenGL_3e.cpp
@@ -6,6 +6,9 @@
 #include<GL/glut.h>
 #include<iostream>
 
+const GLdouble screenWidth = 640;
+const GLdouble screenHeight = 480;
+
 using namespace std;
 
 //<<<<<<<<<<<<<<<<<<<< myInit >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -16,7 +19,7 @@ void myInit() {
 	glPointSize(4.0);					// 设置点的大小为4x4像素
 	glMatrixMode(GL_PROJECTION);		// 设置合适的矩阵
 	glLoadIdentity();					// 后台将解释
-	gluOrtho2D(0.0, 640.0, 0.0, 480.0);	// 接下来将完整的说明
+	gluOrtho2D(0.0, screenWidth, 0.0, screenHeight);	// 接下来将完整的说明
 }
 
 struct GLintPoint {
@@ -48,8 +51,8 @@ void myDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);		// 清屏
 	int width = 10;
 	int height = 10;
-	for (int row = 0;row < 10;++row) {
-		for (int col = 0;col < 10;++col) {
+	for (int row = 0;row < 50;++row) {
+		for (int col = 0;col < 50;++col) {
 			if ((col+row)%2==0) {
 				glColor3f(0.2,0.2,0.2);				// 深灰
 			}
@@ -61,19 +64,40 @@ void myDisplay() {
 	}
 	glFlush();
 }
+
+//<<<<<<<<<<<<<<<<<< drawDot >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+void drawDot(int x,int y) 
+{
+	glBegin(GL_POINTS);
+		glVertex2i(x,y);
+	glEnd();
+}
+//<<<<<<<<<<<<<<<<<< myMouse >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+void myMouse(int button, int state, int x, int y)
+{
+	if (state == GLUT_DOWN) {
+		if (button == GLUT_LEFT_BUTTON) {
+			drawDot(x, screenHeight - y);
+			glFlush();
+		}
+		else if (button == GLUT_RIGHT_BUTTON) {
+			glClearColor(1.0f, 0.0f, 0.0f, 0.0f);		// 红色
+			glClear(GL_COLOR_BUFFER_BIT);
+			glFlush();
+		}
+	}
+}
 int main(int argc, char **argv)
 {
 	// GLUT 初始化函数
 	glutInit(&argc, argv);							// 初始化工具包
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);	// 设置显示模式
-	glutInitWindowSize(640, 480);					// 设置窗口大小
+	glutInitWindowSize(screenWidth, screenHeight);	// 设置窗口大小
 	glutInitWindowPosition(100, 100);				// 设置屏幕上窗口位置
 	glutCreateWindow("My first OpenGL program");	// 打开带标题的窗口
 
 	glutDisplayFunc(myDisplay);						// 注册重画回调函数
-													//glutMouseFunc(myMouse);
-													//glutKeyboardFunc(myKeyboard);
-													//glutReshapeFunc(myShape);
+	glutMouseFunc(myMouse);
 
 	myInit();
 	glutMainLoop();									// 进入循环
